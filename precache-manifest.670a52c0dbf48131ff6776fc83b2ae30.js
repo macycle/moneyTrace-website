32db38ce05db6efc868c6745e6b66d375cbfa7fd
macycle <1124942097@qq.com>
2020-08-12 14:47:02 +0800

    deploy

---
+++ precache-manifest.670a52c0dbf48131ff6776fc83b2ae30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a031c3ad0d03bc5e63c",
+    "url": "/moneyTrace-website/css/app.5e2c8e27.css"
+  },
+  {
+    "revision": "4b93c3d58a4528195f32408e10fc1c56",
+    "url": "/moneyTrace-website/img/logo.4b93c3d5.png"
+  },
+  {
+    "revision": "30d787d53bd94ea36604e3c4c07d4c55",
+    "url": "/moneyTrace-website/index.html"
+  },
+  {
+    "revision": "4a031c3ad0d03bc5e63c",
+    "url": "/moneyTrace-website/js/app.d8e6fa53.js"
+  },
+  {
+    "revision": "b29f0a072d85e607e6b7",
+    "url": "/moneyTrace-website/js/chunk-vendors.3579806c.js"
+  },
+  {
+    "revision": "3d1efc7c752d19c13ac5b8b665a3f580",
+    "url": "/moneyTrace-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneyTrace-website/robots.txt"
+  }
+]);
